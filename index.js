7d236da531eb5314534ff25c1b2d265a72658ec5
Christine Jeon <csjeon28@gmail.com>
2021-04-11 18:14:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function isWin(element, index, array) {
+    return (element.result === "W")
+}
 
+function superbowlWin(record) {
+    let winElement = record.find(isWin);
+    if (winElement === undefined) {
+      return winElement;
+    } else {
+      return winElement.year;
+    }
+}
